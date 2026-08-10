4f542fa5bf4cce0a925ccf31a3a86577a7ed4bd8
Venkey-doki <venkatesdoki@gmail.com>
2026-08-10 22:46:28 +0530

    feat: add authentication routes for signup, login, refresh token, and logout

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { env } from "./config/env";
 import cors from "cors";
-import globalErrorHandler from "./middleware/GlobalError.middleware";
+import globalErrorHandler from "./middlewares/GlobalError.middleware";
+import authRoutes from "./routes/Auth.routes";
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(globalErrorHandler);
 app.get("/health", (req, res) => {
     res.send("Hello, World!");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(env.port, () => {
     console.log(`Server is running on port http://localhost:${env.port}`);
---
+++ backend/src/routes/Auth.routes.ts
@@ -0,0 +1,17 @@
+import asyncRequestHandler from "../utils/AsyncHandler";
+import { Router } from "express";
+import {
+	signup,
+	login,
+	refreshToken,
+	logout,
+} from "../controllers/Auth.contorller";
+
+const router = Router();
+
+router.post("/signup", asyncRequestHandler(signup));
+router.post("/login", asyncRequestHandler(login));
+router.post("/refresh-token", asyncRequestHandler(refreshToken));
+router.post("/logout", asyncRequestHandler(logout));
+
+export default router;
