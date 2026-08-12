6c44a9c69bc6a5cb9f33993a27027f1595c29d68
Venkey-doki <venkatesdoki@gmail.com>
2026-08-12 22:51:04 +0530

    feat: add leaderboard reconstruction script to update Redis leaderboard

---
+++ backend/src/script/reconstructLeaderboard.ts
@@ -0,0 +1,49 @@
+import { prisma } from "../lib/prisma";
+import { leaderBoardKeys, redisClient } from "../lib/redis";
+import { GameMode } from "../../generated/prisma/client";
+
+const reconstructLeaderboard = async () => {
+	for (const mode of Object.values(GameMode)) {
+		const bestPerUser = await prisma.score.groupBy({
+			by: ["userId"],
+			where: {
+				mode: mode,
+			},
+			_max: {
+				clickCount: true,
+			},
+		});
+
+		if (bestPerUser.length <= 0) {
+			continue;
+		}
+
+		const leaderboard = bestPerUser.map((entry) => ({
+			userId: entry.userId,
+			clickCount: entry._max.clickCount!,
+		}));
+
+        await redisClient.del(leaderBoardKeys.global(mode));
+		await redisClient.zadd(
+			leaderBoardKeys.global(mode),
+			...leaderboard.flatMap((entry) => {
+				if (!entry.clickCount) {
+					return [0, entry.userId];
+				}
+				return [entry.clickCount, entry.userId];
+			}),
+		);
+	}
+
+	await prisma.$disconnect();
+};
+
+reconstructLeaderboard()
+	.then(() => {
+		console.log("Leaderboard reconstruction completed.");
+		process.exit(0);
+	})
+	.catch((error) => {
+		console.error("Error reconstructing leaderboard:", error);
+		process.exit(1);
+	});
