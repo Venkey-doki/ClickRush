d7ca44a78eb7c38b0c42ff0a0dc00ce73620cc4d
Venkey-doki <venkatesdoki@gmail.com>
2026-08-15 11:58:38 +0530

    update: increased teh end session grace time

--- backend/src/config/gameMode.ts
+++ backend/src/config/gameMode.ts
@@ -1,11 +1,11 @@
 import { GameMode } from "../../generated/prisma/client.ts";
 
 export const GAME_MODE_DURATION_MS: Record<GameMode, number> = {
-	CLASSIC_60S: 60_000,
-	SPRINT_10S: 10_000,
-	MARATHON_120S: 120_000,
+	CLASSIC_60S: 60000,// 60 seconds
+	SPRINT_10S: 10000,// 10 seconds
+	MARATHON_120S: 120000,// 120 seconds
 };
 
-export const END_SESSION_GRACE_MS = 2_000;
+export const END_SESSION_GRACE_MS = 5000; // 5 seconds grace period after session ends to allow for final click batch processing
 
 export const MAX_HUMAN_CPS = 15;
